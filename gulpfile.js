cca3b0abd8c1770e613f2132c101b0954e08f28c
yamoo9 <yamoo9@naver.com>
2014-12-02 11:49:49 +0900

    Gulp 기본 테스크 등록!
    gulpfile.js를 만들어 처음으로 기본 테스크를 만들어 콘솔로 확인해봤습니다.

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// NPM Modules 호출
+var gulp = require('gulp'); // Gulp 모듈
+
+// Gulp.task()를 사용해 기본(Default) 테스크 정의
+gulp.task('default', function() {
+	// 콘솔(Console)에 메시지 기록(Log)
+	console.log('Gulp 모듈 호출 성공!');
+});
