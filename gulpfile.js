387b77d0a9d6926cd004991a76155c26244d5bb4
yamoo9 <yamoo9@naver.com>
2014-12-20 13:21:22 +0900

    gulp-concat 코드 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-// NPM Modules 호출
-var gulp = require('gulp'); // Gulp 모듈
+// Modules 호출
+var gulp   = require('gulp'),
+		concat = require('gulp-concat');
+
+// Javascript 파일들을 병합
+gulp.task('combine:js', function() {
+	gulp
+		// .src(['./src/domhelper-id.js', './src/domhelper-tag.js'])
+		// .src('./src/*.js')
+		// .src(['./src/domhelper-prevEl.js', './src/*.js'])
+		.src('src/js/libs/**/*.js')
+		.pipe(concat('DOMlibrary.js'))
+		.pipe(gulp.dest('./dist'));
+});
 
 // Gulp.task()를 사용해 기본(Default) 테스크 정의
 gulp.task('default', function() {
 	// 콘솔(Console)에 메시지 기록(Log)
-	console.log('Gulp 모듈 호출 성공!');
+	console.log('gulp default 일이 수행되었습니다.');
 });
