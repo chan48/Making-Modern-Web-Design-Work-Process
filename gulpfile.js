b0b366ee8f85275fefaa1571cb5c398d5ab4b41a
yamoo9 <yamoo9@naver.com>
2014-12-23 13:54:54 +0900

    CSS 업무 추가 및 코드 정리

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 // Modules 호출
-var gulp   = require('gulp'),
-		jshint = require('gulp-jshint'),
-		uglify = require('gulp-uglify'),
-		concat = require('gulp-concat'),
-		rename = require('gulp-rename'),
-		del    = require('del');
+var gulp      = require('gulp'),
+		csslint   = require('gulp-csslint'),
+		concatcss = require('gulp-concat-css'),
+		uglifycss = require('gulp-uglifycss'),
+		jshint    = require('gulp-jshint'),
+		uglify    = require('gulp-uglify'),
+		concat    = require('gulp-concat'),
+		rename    = require('gulp-rename'),
+		del       = require('del');
 
 /**
  * 파일 경로(Path) 환경설정
  */
 var path = {
+	css: {
+		src      : 'src/css/style.css',
+		dest     : 'dist/css/',
+		filename : 'style.css'
+	},
 	js: {
 		src      : 'src/js/libs/**/*.js',
-		dest     : 'dist/',
+		dest     : 'dist/js/',
 		filename : 'DOMlibrary.js'
 	}
 };
@@ -24,8 +32,7 @@ var path = {
 // 	// 콘솔(Console)에 메시지 기록(Log)
 // 	console.log('gulp default 일이 수행되었습니다.');
 // });
-
-gulp.task('default', ['clean', 'scripts']);
+gulp.task('default', ['clean', 'styles', 'scripts']);
 
 /**
  * 폴더/파일 제거
@@ -35,32 +42,69 @@ gulp.task('clean', function() {
 });
 
 /**
- * Javascript
+ * CSS 업무
+ * NPM 설치 모듈: gulp-csslint, gulp-concat-css, gulp-uglifycss
  * 문법 검사 > 병합 > 압축
  */
-gulp.task('scripts', ['js:hint', 'js:concat', 'js:uglify']);
-
-// JS문법 검사
-gulp.task('js:hint', function() {
-	gulp.src( path.js.src )
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
+gulp.task('styles', function() {
+	gulp.src( path.css.src )
+		// 문법검사
+		.pipe(csslint( {'import': false} ))
+		.pipe(csslint.reporter())
+		// 파일 병합
+		.pipe(concatcss( path.css.filename ))
+		// 압축하지 않은 파일 출력
+		.pipe(gulp.dest( path.css.dest ))
+		// 압축
+		.pipe(uglifycss())
+		// 압축한 파일 이름 바꿔 출력
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest( path.css.dest ));
 });
-// JS 병합
-gulp.task('js:concat', function() {
+
+/**
+ * Javascript 업무
+ * 문법 검사 > 병합 > 압축
+ */
+gulp.task('scripts', function() {
 	gulp.src( path.js.src )
+		// 문법검사
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		// 파일 병합
 		.pipe(concat( path.js.filename ))
+		// 압축하지 않은 파일 출력
+		.pipe(gulp.dest( path.js.dest ))
+		// 압축
+		.pipe(uglify())
+		// 압축한 파일 이름 바꿔 출력
+		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest( path.js.dest ));
 });
+
+// gulp.task('scripts', ['js:hint', 'js:concat', 'js:uglify']);
+
+// JS문법 검사
+// gulp.task('js:hint', function() {
+// 	gulp.src( path.js.src )
+// 		.pipe(jshint())
+// 		.pipe(jshint.reporter('jshint-stylish'));
+// });
+// JS 병합
+// gulp.task('js:concat', function() {
+// 	gulp.src( path.js.src )
+// 		.pipe(concat( path.js.filename ))
+// 		.pipe(gulp.dest( path.js.dest ));
+// });
 // JS 압축
-gulp.task('js:uglify', function() {
-	setTimeout(function () { // 0.2초 뒤에 실행
-		gulp.src( path.js.dest + path.js.filename )
-			.pipe(uglify())
-			.pipe(rename({suffix: '.min'}))
-			.pipe(gulp.dest( path.js.dest ));
-	}, 200);
-});
+// gulp.task('js:uglify', function() {
+// 	setTimeout(function () { // 0.2초 뒤에 실행
+// 		gulp.src( path.js.dest + path.js.filename )
+// 			.pipe(uglify())
+// 			.pipe(rename({suffix: '.min'}))
+// 			.pipe(gulp.dest( path.js.dest ));
+// 	}, 200);
+// });
 
 // gulp.task('scripts', function() {
 // 	gulp
